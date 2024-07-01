17db7186a6d608f5f26a233d8afe34730ae8308c
Cristian Rojas <cristianrdev@gmail.com>
2024-06-30 21:05:56 -0300

    se agrega header, footer, y component home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { PropietarioFormComponent } from './components/propietario-form/propietario-form.component';
 import { PropietarioListaComponent } from './components/propietario-lista/propietario-lista.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
 
     {path: 'propietario-form', component:PropietarioFormComponent},
     {path: 'propietario-form/:id', component:PropietarioFormComponent},
     {path: 'propietario-lista', component:PropietarioListaComponent},
+    { path: 'home', component: HomeComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
 
 ];
