a3c67d7e2ee4ee77ad6d8d4e94a8a0ed21cb36ec
QuispeLuis <luisqtic@gmail.com>
2024-07-03 19:44:41 -0300

    CRUD-Usuario-(component-serviec-model)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,13 +2,19 @@ import { Routes } from '@angular/router';
 import { PropietarioFormComponent } from './components/propietario-form/propietario-form.component';
 import { PropietarioListaComponent } from './components/propietario-lista/propietario-lista.component';
 import { HomeComponent } from './components/home/home.component';
+import { UsuarioListaComponent } from './components/usuario-lista/usuario-lista.component';
+import { UsuarioFormComponent } from './components/usuario-form/usuario-form.component';
 
 export const routes: Routes = [
 
     {path: 'propietario-form', component:PropietarioFormComponent},
     {path: 'propietario-form/:id', component:PropietarioFormComponent},
     {path: 'propietario-lista', component:PropietarioListaComponent},
+    {path: 'usuario-lista', component:UsuarioListaComponent},
+    {path: 'usuario-form', component:UsuarioFormComponent},
+    {path: 'usuario-form/:id', component:UsuarioFormComponent},
+
     { path: 'home', component: HomeComponent },
-    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+    { path: '**', pathMatch: 'full', redirectTo: 'home' },
 
 ];
---
+++ frontend/src/app/components/usuario-form/usuario-form.component.ts
@@ -0,0 +1,102 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Usuario } from '../../models/usuario';
+import { Rol } from '../../models/rol';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UsuarioService } from '../../services/usuario.service';
+import { RolService } from '../../services/rol.service';
+
+@Component({
+  selector: 'app-usuario-form',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './usuario-form.component.html',
+  styleUrl: './usuario-form.component.css'
+})
+export class UsuarioFormComponent {
+
+  accion: string = 'new';
+  usuario!: Usuario;
+  roles!: Array<Rol>;
+
+  constructor(private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private usuarioService: UsuarioService,
+    private rolService: RolService
+  ) {
+    this.iniciarVariable();
+    this.cargarRoles();
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      if (params['id'] == '0') {
+        this.accion = 'new';
+        this.iniciarVariable();
+      } else {
+        this.accion = 'update';
+        //this.cargarUsuario(params['id']);
+      }
+    });
+  }
+
+  iniciarVariable(): void {
+    this.usuario = new Usuario();
+    this.roles = new Array<Rol>();
+  }
+
+  cargarRoles() {
+    this.roles = new Array<Rol>();
+    this.rolService.getRoles().subscribe(
+      result => {
+        let vrol: Rol = new Rol();
+        result.forEach((element: any) => {
+          Object.assign(vrol, element);
+          this.roles.push(vrol);
+          vrol = new Rol();
+        });
+      }
+    )
+  }
+
+  agregarUsuario(){
+    this.usuarioService.addUsuario(this.usuario).subscribe(
+      result => {
+        if (result. status ==  1){
+          alert("Usuario registrado con exito");
+          this.router.navigate(['usuario-lista'])
+        }
+      },
+      (error: any) => {
+        alert("Error al registrar Usuario");
+        console.log(error);
+      }
+    )
+    //this.usuario = new Usuario();
+  }
+
+  acualizarUsuario(){
+    this.usuarioService.updateUsuario(this.usuario).subscribe(
+      result => {
+        if (result. status ==  1){
+          alert("Usuario actualizado con exito");
+          this.router.navigate(['usuario-lista'])
+        }
+      },
+      (error: any) => {
+        alert("Error al actualizar Usuario");
+        console.log(error);
+      }
+    )
+    //this.usuario = new Usuario();
+  }
+
+  atras(){
+    this.router.navigate(['usuario-lista'])
+  }
+
+}
+
+
+
