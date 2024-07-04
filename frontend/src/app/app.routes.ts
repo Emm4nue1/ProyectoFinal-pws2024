a5ca2eeae1f7db29799cb657088997761b98b43a
Emm4nue1 <ema.mendoza97@outlook.com>
2024-07-04 00:35:20 -0300

    cambios rapidos en la parte visual

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { UsuarioListaComponent } from './components/usuario-lista/usuario-lista.
 import { UsuarioFormComponent } from './components/usuario-form/usuario-form.component';
 import { PromocionFormComponent } from './components/promocion-form/promocion-form.component';
 import { PromocionListaComponent } from './components/promocion-lista/promocion-lista.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
 
@@ -15,9 +16,10 @@ export const routes: Routes = [
     {path: 'usuario-lista', component:UsuarioListaComponent},
     {path: 'usuario-form', component:UsuarioFormComponent},
     {path: 'usuario-form/:id', component:UsuarioFormComponent},
-    { path: 'promocion-form', component: PromocionFormComponent},
-    { path: 'promocion-form/:id', component: PromocionFormComponent},
-    { path: 'promocion-lista', component: PromocionListaComponent},
+    {path: 'promocion-form', component: PromocionFormComponent},
+    {path: 'promocion-form/:id', component: PromocionFormComponent},
+    {path: 'promocion-lista', component: PromocionListaComponent},
+    {path: 'login', component:LoginComponent}, 
 
     { path: 'home', component: HomeComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'home' },
