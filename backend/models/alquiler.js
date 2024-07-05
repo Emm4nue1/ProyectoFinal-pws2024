847999122614ff56f38cd5273980d1134c02521d
QuispeLuis <luisqtic@gmail.com>
2024-07-05 00:35:21 -0300

    Alquiles-model-service

--- backend/models/alquiler.js
+++ backend/models/alquiler.js
@@ -4,7 +4,7 @@ const Local = require ('./local')
 const Usuario = require ('./usuario')
 
 const AlquilerSchema = new Schema({
-    propietario: { type: Schema.Types.ObjectId, ref: Usuario, required: true },
+    usuario: { type: Schema.Types.ObjectId, ref: Usuario, required: true },
     local: { type: Schema.Types.ObjectId, ref: Local, required: true },
     plazomes: { type: Number, required: true },
     costoalquiler: { type: Number, required: true },
