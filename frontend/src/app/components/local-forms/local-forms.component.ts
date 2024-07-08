aa61c46a59e47de293a7465210106716db406ad0
manukito2012 <emanuelguanuco17@gmail.com>
2024-07-08 12:02:29 -0300

    Creacion de componentes Local y Local-forms- Modificacion de Headers

---
+++ frontend/src/app/components/local-forms/local-forms.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-local-forms',
+  standalone: true,
+  imports: [],
+  templateUrl: './local-forms.component.html',
+  styleUrl: './local-forms.component.css'
+})
+export class LocalFormsComponent {
+
+}
---
+++ frontend/src/app/components/local/local.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { Local } from '../../models/local';
+import { Router } from '@angular/router';
+import { LocalService } from '../../services/local.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-local',
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  templateUrl: './local.component.html',
+  styleUrl: './local.component.css'
+})
+export class LocalComponent {
+
+
+  locales: Array<Local> = [];
+
+  constructor( private router:Router , private localService:LocalService ) {
+    this.verLocales();
+
+  }
+
+
+  verLocales():void{
+
+    this.localService.getLocales().subscribe(
+      result => {
+        console.log(result);
+        this.locales=result;
+      })
+  }
+
+
+  modificarLocal(_id:string){
+    this.router.navigate(['local-forms', _id]);
+  }
+
+  agregarLocal() {
+    this.router.navigate(['local-forms', "0"]);
+  }
+
+
+}
