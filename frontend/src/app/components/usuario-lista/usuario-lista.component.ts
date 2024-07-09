02a0d7fdd5e685dd964637443e5b76be23c6f635
QuispeLuis <luisqtic@gmail.com>
2024-07-09 02:51:56 -0300

    Filtro Usuarios

--- frontend/src/app/components/usuario-lista/usuario-lista.component.ts
+++ frontend/src/app/components/usuario-lista/usuario-lista.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { RolService } from '../../services/rol.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Rol } from '../../models/rol';
 
 
 @Component({
@@ -18,16 +19,30 @@ export class UsuarioListaComponent {
 
   usuarios: Array<Usuario>;
 
+  roles!: Array<Rol>;
+  apellidoAux: string = "";
+  rolAux: string = "";
+  dniAux: number=0;
 
   constructor(private router: Router,
-              private usuarioService: UsuarioService 
+    private usuarioService: UsuarioService,
+    private rolService: RolService
   ) {
     this.usuarios = new Array<Usuario>();
-    this.getUsuarios();
+    this.getUsuarios("", "",0);
+    this.cargarRoles();
   }
 
-  getUsuarios(): void {
-    this.usuarioService.getUsuarios().subscribe(
+  buscarYLimpiar(): void {
+    this.getUsuarios(this.apellidoAux, this.rolAux, this.dniAux);
+    this.limpiarCampos();
+  }
+  getUsuarios(apellidoAux: string, rolAux: string, dniAux: number): void {
+    //console.log(rolAux);
+    if (rolAux == "Todos"){
+      rolAux = "";
+    }
+    this.usuarioService.getUsuariosFiltros(apellidoAux, rolAux, dniAux ).subscribe(
       (result: any) => {
         this.usuarios = result;
         //console.log(this.usuarios);
@@ -37,12 +52,18 @@ export class UsuarioListaComponent {
       }
     );
   }
+    // Método para limpiar los campos
+    limpiarCampos(): void {
+      this.rolAux = '';
+      this.apellidoAux = '';
+      this.dniAux = 0 ;
+    }
 
-  agregarUsuario(){
-    this.router.navigate(['usuario-form','0']);
+  agregarUsuario() {
+    this.router.navigate(['usuario-form', '0']);
   }
 
-  modificarUsuario(usuario: Usuario){
+  modificarUsuario(usuario: Usuario) {
     //console.log(usuario);
     this.router.navigate(['usuario-form', usuario._id]); usuario
   }
@@ -50,7 +71,7 @@ export class UsuarioListaComponent {
   eliminarUsuario(usuario: Usuario): void {
     this.usuarioService.deleteUsuario(usuario).subscribe(
       (result: any) => {
-        if(result.status == 1){
+        if (result.status == 1) {
           alert('Usuario eliminado correctamente');
           location.reload()
         }
@@ -59,6 +80,20 @@ export class UsuarioListaComponent {
         console.error('Error:', error);
       }
     );
+  }
+
+  cargarRoles() {
+    this.roles = new Array<Rol>();
+    this.rolService.getRoles().subscribe(
+      result => {
+        let vrol: Rol = new Rol();
+        result.forEach((element: any) => {
+          Object.assign(vrol, element);
+          this.roles.push(vrol);
+          vrol = new Rol();
+        });
+      }
+    )
   }
 
 }
