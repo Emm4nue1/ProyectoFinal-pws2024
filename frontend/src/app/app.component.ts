1bdbe0df1ab47a45e7d0d4fb6937c6a87f6af233
Emm4nue1 <ema.mendoza97@outlook.com>
2024-07-09 04:51:05 -0300

    cambios sutiles

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,14 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
-import { PromocionFormComponent } from './components/promocion-form/promocion-form.component';
-import { PromocionListaComponent } from './components/promocion-lista/promocion-lista.component';
-import { FacebookModule } from 'ngx-facebook';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginComponent,PromocionFormComponent,PromocionListaComponent,FacebookModule.forRoot()],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
