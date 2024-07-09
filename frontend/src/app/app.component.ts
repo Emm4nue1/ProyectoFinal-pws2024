0eae1c83fbf3bad3bca9c0a032f39d13c9f34520
Emm4nue1 <ema.mendoza97@outlook.com>
2024-07-09 03:40:40 -0300

    Implementacion de fb

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
+import { PromocionFormComponent } from './components/promocion-form/promocion-form.component';
+import { PromocionListaComponent } from './components/promocion-lista/promocion-lista.component';
+import { FacebookModule } from 'ngx-facebook';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginComponent,PromocionFormComponent,PromocionListaComponent,FacebookModule.forRoot()],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
